342ff23245a58ecfc4d35cc7b6bc5a3ac6b35bfc
CIANDT\igabriel <igabriel@ciandt.com>
2022-06-21 10:49:08 -0300

    SOLID

---
+++ 0x03/nomenclatura/Name.java
@@ -0,0 +1,27 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+
+    public Date modifiedDate;
+
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+
+    public List<Account> AccountList;
+
+    public void deleteUser(Name.User user){};
+
+    public URL url;
+
+    public class TableRepresent {};
+
+    public void saveCar(){};
+
+    public saveUser(){}; saveAccount(){}; saveContract(){};
+
+    public final int DAYS_IN_YEAR = 360;
+    
+    public final int HOURS_IN_WEEK = 168;
+
+}
